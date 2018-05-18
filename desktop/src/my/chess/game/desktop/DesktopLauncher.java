23e6f90d188784dbb944a565cb9a28e67c82605d
Patrick Dahlin <patrick.dahlin@gmail.com>
2018-05-18 20:14:29 +0300

    Initial commit

---
+++ desktop/src/my/chess/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package my.chess.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import my.chess.game.BasicChess;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BasicChess(), config);
+	}
+}
