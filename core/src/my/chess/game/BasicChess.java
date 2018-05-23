19291dd36e759d60bf3b232230e2d4957c797252
Patrick Dahlin <patrick.dahlin@gmail.com>
2018-05-23 22:48:12 +0300

    Work on chess model

--- core/src/my/chess/game/BasicChess.java
+++ core/src/my/chess/game/BasicChess.java
@@ -26,27 +26,28 @@ Schackregler, kontroll av lagliga flyttningar, kontroll av vinst/förlust. Kontr
 
 
 public class BasicChess extends ApplicationAdapter {
+
 	SpriteBatch batch;
 	Texture img;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		//img = new Texture("badlogic.jpg");
+		img = new Texture("chessboard.png");
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		//batch.draw(img, 0, 0);
+		batch.draw(img, 0, 0);
 		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		//img.dispose();
+		img.dispose();
 	}
 }
---
+++ core/src/my/chess/logic/ChessBoard.java
@@ -0,0 +1,19 @@
+package my.chess.logic;
+
+
+class ChessBoard {
+	
+	ChessPiece[] player1;
+	ChessPiece[] player2;
+	boolean isPlaying;
+	int playerTurn = 1; // Player 1 => 1, Player 2 => 2
+	
+	//void MovePieceTo(int playerNr, ChessPiece p, int toX, int toY)
+	//void MovePieceToLegal(int playerNr, ChessPiece p, int toX, int toY)
+	//ChessPiece GetPieceAt(int x, int y)
+
+	
+	
+	// Win/Lost listeners, moves might trigger win/lost scenario
+	
+}
