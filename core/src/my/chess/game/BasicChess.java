93084b7be5953616648e4a8b6d4981d0df37a22b
Patrick Dahlin <patrick.dahlin@gmail.com>
2018-05-23 21:56:58 +0300

    Added KryoNet and image assets

--- core/src/my/chess/game/BasicChess.java
+++ core/src/my/chess/game/BasicChess.java
@@ -6,6 +6,25 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+
+/*
+ Moment 1
+Schackets underliggande datamodell: schackbrädan, pjäser med positioner, m.m.
+
+Moment 2
+GUI
+
+Moment 3
+Möjlighet att ”spela” schack-spelet, dvs initialisering av spelet, flytta på pjäser
+
+Moment 4
+Datakommunikation mellan datorerna, dvs skicka och ta emot data, tolka data och uppdatera spelet utgående från emottagen data
+
+Moment 5
+Schackregler, kontroll av lagliga flyttningar, kontroll av vinst/förlust. Kontroll av vems tur som är på gång, dvs den vars tur det inte är skall inte kunna flytta på pjäser. 
+*/
+
+
 public class BasicChess extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
